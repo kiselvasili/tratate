932191af5bec77c19a001d5eb4797ab4d6df68f8
kisel_vasili <kiselvasili@gmail.com>
2017-02-21 14:04:04 +0300

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-main',
+    template: '<h1>Hello</h1>'
+})
+
+export class AppComponent{}
---
+++ app/app.module.ts
@@ -0,0 +1,8 @@
+'use strict'
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent} from './app.component'
+
+@NgModule({})
