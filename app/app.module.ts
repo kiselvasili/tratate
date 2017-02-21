bbcb3cb18744eb3518c311850dcd18a5a7d834f9
kiselvasili <kiselvasili@gmail.com>
2017-02-21 15:07:12 +0300

    second commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,12 @@
-'use strict'
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent} from './app.component'
 
-@NgModule({})
+@NgModule({
+    imports:      [ BrowserModule, FormsModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
+})
+
+export class AppModule {}
