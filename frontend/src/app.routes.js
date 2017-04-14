777ad47e59e1c75fca2678d2281a1097e844ab0c
kisel_vasili <kiselvasili@gmail.com>
2017-04-14 12:55:09 +0300

    delete

--- frontend/src/app.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var router_1 = require('@angular/router');
-var login_component_1 = require('./login/login.component');
-var APP_ROUTES = [
-    { path: 'login', component: login_component_1.LoginComponent },
-    { path: '**', redirectTo: 'login' }
-];
-exports.routing = router_1.RouterModule.forRoot(APP_ROUTES);
-//# sourceMappingURL=app.routes.js.map
